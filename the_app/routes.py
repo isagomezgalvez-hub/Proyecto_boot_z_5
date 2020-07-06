e2459b9cc337a8c9a08f2650b4925785557b70c8
Isabel Gomez <isa@MacBook-Pro-de-Isabel.local>
2020-07-06 17:41:53 +0200

    funcion consultaSaldo

--- the_app/routes.py
+++ the_app/routes.py
@@ -7,34 +7,6 @@ from datetime import datetime
 import requests, sqlite3,json
 
 
-def SaldoCrypto():
-		conn = sqlite3.connect(app.config['BASE_DATOS'])
-		cur = conn.cursor()
-
-		Saldo = "SELECT sum(to_quantity) from compras WHERE from_quantity = '{}'".format(request.values.get('MonedaFrom'))
-		cantidadFrom=cur.execute(Saldo).fetchall()
-		saldoFrom = cantidadFrom[0]
-
-		Saldo = "SELECT sum(to_currency) from compras WHERE from_currency = '{}'".format(request.values.get('MonedaFrom'))
-		cantidadTo=cur.execute(Saldo).fetchall()
-		saldoTo = cantidadTo[0]
-		
-		if saldoFrom[0] is not None or saldoTo[0] is not None:
-
-					if saldoFrom[0] and saldoTo[0] is not None:
-						saldo = float(saldoFrom[0] - saldoTo[0])
-
-					elif saldoFrom[0] == None and saldoTo[0] is not None:
-						saldo = float(saldoTo[0])
-					
-					else:
-						saldo = float(saldoFrom[0])
-
-		return saldo
-		conn.close()
-		
-		
-
 @app.route("/")
 def index():
 	conn = sqlite3.connect(app.config['BASE_DATOS'])
@@ -127,15 +99,34 @@ def purchase():
 							conn = sqlite3.connect(app.config['BASE_DATOS'])
 							cur = conn.cursor()
 
-							consultaSaldo = SaldoCrypto()
-							cantidadCompra = float(request.values.get('Q_Form'))
+							Saldo = "SELECT sum(to_quantity) from compras WHERE from_quantity = '{}'".format(request.values.get('MonedaFrom'))
+							cantidadFrom=cur.execute(Saldo).fetchall()
+							saldoFrom = cantidadFrom[0]
 
-							if consultaSaldo > cantidadCompra:
+							Saldo = "SELECT sum(to_currency) from compras WHERE from_currency = '{}'".format(request.values.get('MonedaFrom'))
+							cantidadTo=cur.execute(Saldo).fetchall()
+							saldoTo = cantidadTo[0]
+
+							if saldoFrom[0] is not None or saldoTo[0] is not None:
+
+								if saldoFrom[0] and saldoTo[0] is not None:
+									saldo = saldoFrom[0] - saldoTo[0]
+
+								if saldoFrom[0] == None and saldoTo[0] is not None:
+									saldo = saldoTo[0]
+
+								else:
+									saldo = saldoFrom[0]
+
+							Cantidad = float(request.values.get('Q_Form'))
+							
+							if saldo > Cantidad:
 								query = "INSERT INTO compras (date,time,from_currency,from_quantity,to_currency,to_quantity,P_U) values (?,?,?,?,?,?,?);"
 								datos =(now.date(),time,request.values.get('MonedaFrom'), request.values.get('MonedaTo'), request.values.get('Q_Form'),round(float(form.Q_to.data), 8),round(float(form.P_U.data), 8))
 							else:
 								errorsaldo = ('Saldo insuficiente. No tienes suficiente cantidad de {} para comprar: {} - {}. Por favor, intentalo con una cantidad menor o con otra criptomoneda de la que disponga con más saldo.'.format(request.values.get('MonedaFrom'),request.values.get('Q_Form'),request.values.get('MonedaTo')))
 								return render_template('compras.html',form=form, error_saldo=errorsaldo)
+							
 							try:
 									cur.execute(query,datos)
 									conn.commit()
