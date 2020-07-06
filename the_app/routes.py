4e09893c6bde04af8c22bed9415765e38aae6aa7
Isabel Gomez <isa@MacBook-Pro-de-Isabel.local>
2020-07-06 21:28:47 +0200

    Redefinitivo

--- the_app/routes.py
+++ the_app/routes.py
@@ -93,7 +93,6 @@ def purchase():
 						 return render_template('compras.html', form=form, calculaPrecio=error)
 					
 					else:
-						
 						if form.validate():		
 	
 							conn = sqlite3.connect(app.config['BASE_DATOS'])
@@ -103,48 +102,69 @@ def purchase():
 							cantidadFrom=cur.execute(Saldo).fetchall()
 							saldoFrom = cantidadFrom[0]
 
-							Saldo = "SELECT sum(to_currency) from compras WHERE from_currency!= 'EUR' and from_currency = '{}'".format(request.values.get('MonedaFrom'))
+							Saldo = "SELECT sum(to_currency) from compras WHERE from_currency != 'EUR' and from_currency = '{}'".format(request.values.get('MonedaFrom'))
 							cantidadTo=cur.execute(Saldo).fetchall()
 							saldoTo = cantidadTo[0]
 
-					
+							hayMoneda = "SELECT from_quantity from compras WHERE from_quantity = '{}'".format(request.values.get('MonedaFrom'))
+							monedas	= cur.execute(hayMoneda).fetchall()
+							
+							if request.values.get('MonedaFrom') == 'EUR':
 
-							if saldoFrom[0] is not None or saldoTo[0] is not None:
+								query = "INSERT INTO compras (date,time,from_currency,from_quantity,to_currency,to_quantity,P_U) values (?,?,?,?,?,?,?);"
+								datos =(now.date(),time,request.values.get('MonedaFrom'), request.values.get('MonedaTo'), request.values.get('Q_Form'),round(float(form.Q_to.data), 8),round(float(form.P_U.data), 8))
 
-									if saldoFrom[0] and saldoTo[0] is not None:
-										saldo = saldoFrom[0] - saldoTo[0]
+								try:
+										cur.execute(query,datos)
+										conn.commit()
+										return redirect(url_for("index"))	
 
-									if saldoFrom[0] == None and saldoTo[0] is not None:
-										saldo = saldoTo[0]
-
-									else:
-										saldo = saldoFrom[0]
-
-									Cantidad = float(request.values.get('Q_Form'))
-								
-									if saldo >= Cantidad:
-										query = "INSERT INTO compras (date,time,from_currency,from_quantity,to_currency,to_quantity,P_U) values (?,?,?,?,?,?,?);"
-										datos =(now.date(),time,request.values.get('MonedaFrom'), request.values.get('MonedaTo'), request.values.get('Q_Form'),round(float(form.Q_to.data), 8),round(float(form.P_U.data), 8))
-									else:
-										errorsaldo = ('Saldo insuficiente. No tienes suficiente cantidad de {} para comprar: {} - {}. Por favor, intentalo con una cantidad menor o con otra criptomoneda de la que disponga con más saldo.'.format(request.values.get('MonedaFrom'),request.values.get('Q_Form'),request.values.get('MonedaTo')))
-										return render_template('compras.html',form=form, error_saldo=errorsaldo)
+								except Exception as e:
+										errorbd = ('Error de acceso a la base de datos: {}'.format(e))
+										return render_template('compras.html',form=form, error_bd=errorbd)
+										
+								conn.close()
+							
 							else:
-									query = "INSERT INTO compras (date,time,from_currency,from_quantity,to_currency,to_quantity,P_U) values (?,?,?,?,?,?,?);"
-									datos =(now.date(),time,request.values.get('MonedaFrom'), request.values.get('MonedaTo'), request.values.get('Q_Form'),round(float(form.Q_to.data), 8),round(float(form.P_U.data), 8))
-							
+								for moneda in monedas:
+									
+									if moneda != 'EUR':
+										if saldoFrom[0] is not None or saldoTo[0] is not None:
 
-							try:
-									cur.execute(query,datos)
-									conn.commit()
-									return redirect(url_for("index"))	
+												if saldoFrom[0] and saldoTo[0] != None:
+													saldo = saldoFrom[0] - saldoTo[0]
+
+												elif saldoFrom[0] == None and saldoTo[0] is not None:
+													saldo = saldoTo[0]
+
+												elif saldoFrom[0] is not None and saldoTo[0]== None:
+													saldo = saldoFrom[0]
+
+										Cantidad = float(request.values.get('Q_Form'))
+											
+										if saldo >= Cantidad:
+												query = "INSERT INTO compras (date,time,from_currency,from_quantity,to_currency,to_quantity,P_U) values (?,?,?,?,?,?,?);"
+												datos =(now.date(),time,request.values.get('MonedaFrom'), request.values.get('MonedaTo'), request.values.get('Q_Form'),round(float(form.Q_to.data), 8),round(float(form.P_U.data), 8))
+												
+												
+										else:
+											errorsaldo = ('Saldo insuficiente. No tienes suficiente cantidad de {} para comprar: {} - {}. Por favor, intentalo con una cantidad menor o con otra criptomoneda de la que disponga con más saldo.'.format(request.values.get('MonedaFrom'),request.values.get('Q_Form'),request.values.get('MonedaTo')))
+											return render_template('compras.html',form=form, error_saldo=errorsaldo)
+									try:
+										cur.execute(query,datos)
+										conn.commit()
+										return redirect(url_for("index"))	
+
+									except Exception as e:
+										errorbd = ('Error de acceso a la base de datos: {}'.format(e))
+										return render_template('compras.html',form=form, error_bd=errorbd)
+											
+									conn.close()
 								
+								errormoneda = ('No tienes saldo de esta moneda, intenta comprar con alguna moneda de las que dispongas saldo')
+								return render_template('compras.html',form=form, error_moneda=errormoneda)
 
-							except Exception as e:
-									errorbd = ('Error de acceso a la base de datos: {}'.format(e))
-									return render_template('compras.html',form=form, error_bd=errorbd)
 							
-							conn.close()
-						
 						else:
 							return render_template('compras.html',form=form)
 
